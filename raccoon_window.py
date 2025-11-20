1903f412235f51e5b8870e773b4583d8b43d8f69
Emma Liang <emmaliang333@gmail.com>
2025-11-20 07:44:22 -0800

    Added raccoon!!!!!!!!!!

--- raccoon_window.py
+++ raccoon_window.py
@@ -4,38 +4,48 @@ import os
 
 impath = os.path.join(os.path.dirname(__file__), 'image', '') #image directory
 x = 1400
-
+y = 700
+gif_dimension = 128
 pet_window = tk.Tk()
+state = "sad_pomodoro"
+gif = None
 
 # call buddy's action gif
-walk_positive = [tk.PhotoImage(file=impath + 'walking_positive.gif', format='gif -index %i' % (i)) for i in
-                 range(8)]  # walk to left gif
 raccoon_knitting = [tk.PhotoImage(file=impath + 'raccoon_knitting.gif', format='gif -index %i' % (i)) for i in range(2)]
+raccoon_idle = [tk.PhotoImage(file=impath + 'raccoon_idle.gif', format='gif -index %i' % (i)) for i in range(2)]
+raccoon_sad_idle = [tk.PhotoImage(file=impath + 'raccoon_sad.gif', format='gif -index %i' % (i)) for i in range (3)]
+raccoon_sad_knitting = [tk.PhotoImage(file=impath + 'raccoon_sad_knitting.gif', format='gif -index %i' % (i)) for i in range (2)]
+
+# making gif loop through frames
 frame = 0
-# making gif work
 def play_gif(frames):
     global frame
     pet_label.configure(image=frames[frame])
     frame = (frame + 1) % len(frames)
-    pet_window.after(1000, play_gif, frames)
-
+    delay = 1000
+    if state == "sad_pomodoro":
+        delay = 1800
+    elif state == "sad_idle":
+        delay = 500
+    pet_window.after(delay, play_gif, frames)
 
 def on_right_click(event):
     print("RIGHT CLICK detected")  # test line
 
     # Create popup menu
     menu = tk.Menu(pet_window, tearoff=0)
-    #menu.add_command(label="Sleep", command=lambda: change_state(1, 5))
+    # menu.add_command(label="Sleep", command=lambda: change_state(1, 5))
+    if state == "pomodoro" or state == "sad_pomodoro":
+        menu.add_command(label="Pause Pomodoro", command=pet_window.quit)
+        menu.add_command(label="End Pomodoro", command=pet_window.quit)
+    else: #for idle and sad idle
+        menu.add_command(label="Start Pomodoro", command=pet_window.quit)
     menu.add_separator()
     menu.add_command(label="Close", command=pet_window.quit)
     menu.post(event.x_root, event.y_root)
 
-
-# Bind click event to the label
-
-pet_window.geometry('128x128+' + str(x) + '+7 00')  # imagine border
-# window configuration
-#pet_window.config(highlightbackground='black')
+#window and label configuration and binding
+pet_window.geometry(str(gif_dimension)+ 'x' +str(gif_dimension) + '+' + str(x) + '+' + str(y))  # dimensions and positioning of pet
 pet_window.overrideredirect(True)
 pet_window.wm_attributes('-transparentcolor', 'red')
 pet_window.wm_attributes('-topmost', True)  # Make it top of everything
@@ -43,6 +53,16 @@ pet_window.wm_attributes('-topmost', True)  # Make it top of everything
 pet_label = tk.Label(pet_window, bd=0, bg='black')
 pet_label.bind('<Button-3>', on_right_click)  # Right click
 pet_label.pack()
-play_gif(raccoon_knitting)
+
+# play gifs
+if state == "pomodoro":
+    gif = raccoon_knitting
+elif state == "idle":
+    gif = raccoon_idle
+elif state == "sad_idle":
+    gif = raccoon_sad_idle
+elif state == "sad_pomodoro":
+    gif = raccoon_sad_knitting
+play_gif(gif)
 pet_window.mainloop()
 
