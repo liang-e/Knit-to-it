c8e9e5300d4160e0346a252d1f89dcbcc3c95a91
Emma Liang <emmaliang333@gmail.com>
2025-11-19 21:30:19 -0800

    Added raccoon!!!!!!!!!!

--- raccoon_window.py
+++ raccoon_window.py
@@ -10,13 +10,14 @@ pet_window = tk.Tk()
 # call buddy's action gif
 walk_positive = [tk.PhotoImage(file=impath + 'walking_positive.gif', format='gif -index %i' % (i)) for i in
                  range(8)]  # walk to left gif
+raccoon_knitting = [tk.PhotoImage(file=impath + 'raccoon_knitting.gif', format='gif -index %i' % (i)) for i in range(2)]
 frame = 0
 # making gif work
 def play_gif(frames):
     global frame
     pet_label.configure(image=frames[frame])
-    frame = (frame +1) % len(frames)
-    pet_window.after(100, play_gif, frames)
+    frame = (frame + 1) % len(frames)
+    pet_window.after(1000, play_gif, frames)
 
 
 def on_right_click(event):
@@ -32,16 +33,16 @@ def on_right_click(event):
 
 # Bind click event to the label
 
-pet_window.geometry('100x100+' + str(x) + '+500')  # imagine border
+pet_window.geometry('128x128+' + str(x) + '+7 00')  # imagine border
 # window configuration
-pet_window.config(highlightbackground='black')
+#pet_window.config(highlightbackground='black')
 pet_window.overrideredirect(True)
-pet_window.wm_attributes('-transparentcolor', 'black')
+pet_window.wm_attributes('-transparentcolor', 'red')
 pet_window.wm_attributes('-topmost', True)  # Make it top of everything
 
 pet_label = tk.Label(pet_window, bd=0, bg='black')
 pet_label.bind('<Button-3>', on_right_click)  # Right click
 pet_label.pack()
-play_gif(walk_positive)
+play_gif(raccoon_knitting)
 pet_window.mainloop()
 
