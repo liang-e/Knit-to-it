11bc87da97ed791a8212af11ee42f4ce18d4fe35
Emma Liang <emmaliang333@gmail.com>
2025-11-23 14:57:25 -0800

    Made raccoon window into a class with changing states

--- raccoon_window.py
+++ raccoon_window.py
@@ -2,67 +2,82 @@
 import tkinter as tk
 import os
 
-impath = os.path.join(os.path.dirname(__file__), 'image', '') #image directory
-x = 1400
-y = 700
-gif_dimension = 128
+class RaccoonWindow:
+
+    def __init__(self, window, x=1400, y=700, dimension=128):
+
+        self.impath = os.path.join(os.path.dirname(__file__), 'image', '') #image directory
+        self.raccoon_knitting = [tk.PhotoImage(file=self.impath + 'raccoon_knitting.gif', format='gif -index %i' % i)
+                                 for i in range(2)]
+        self.raccoon_idle = [tk.PhotoImage(file=self.impath + 'raccoon_idle.gif', format='gif -index %i' % i) for i in
+                             range(2)]
+        self.raccoon_sad_idle = [tk.PhotoImage(file=self.impath + 'raccoon_sad.gif', format='gif -index %i' % i) for i
+                                 in range(3)]
+        self.raccoon_sad_knitting = [
+            tk.PhotoImage(file=self.impath + 'raccoon_sad_knitting.gif', format='gif -index %i' % i) for i in range(2)]
+
+        self.x = x
+        self.y = y
+        self.dimension = dimension
+        self.window = window
+        self.state = "idle"
+        self.gif = self.raccoon_idle
+        self.frame = 0
+
+        # window and label configuration and binding
+        self.window.geometry(str(self.dimension) + 'x' + str(self.dimension) + '+' + str(self.x) + '+' + str(self.y))  # dimensions and positioning of pet
+        self.window.overrideredirect(True)
+        self.window.wm_attributes('-transparentcolor', 'red')
+        self.window.wm_attributes('-topmost', True)  # Make it top of everything
+
+        self.label = tk.Label(pet_window, bd=0, bg='black')
+        self.label.bind('<Button-3>', self.on_right_click)  # Right click
+        self.label.pack()
+
+        self.__play_gif(self.state)
+
+    # making gif loop through frames
+    def __play_gif(self, state):
+        print(state)
+        self.label.configure(image=self.gif[self.frame])
+        self.frame = (self.frame + 1) % len(self.gif)
+        delay = 1000
+        if self.state == "sad_pomodoro":
+            delay = 1800
+        elif self.state == "sad_idle":
+            delay = 500
+        pet_window.after(delay, self.__play_gif, self.state)
+
+    def __set_state(self, state):
+        self.state = state
+        if self.state == "pomodoro":
+            self.gif = self.raccoon_knitting
+        elif state == "idle":
+            self.gif = self.raccoon_idle
+        elif state == "sad_idle":
+            self.gif = self.raccoon_sad_idle
+        elif state == "sad_pomodoro":
+            self.gif = self.raccoon_sad_knitting
+
+    def on_right_click(self, event):
+        # Create popup menu
+        menu = tk.Menu(pet_window, tearoff=0)
+        # menu.add_command(label="Sleep", command=lambda: change_state(1, 5))
+        if self.state == "pomodoro" or self.state == "sad_pomodoro":
+            menu.add_command(label="Pause Pomodoro", command=lambda: self.__set_state("sad_idle"))
+            menu.add_command(label="End Pomodoro", command=lambda: self.__set_state("sad_idle"))
+        elif self.state == "sad_idle":  # sad idle
+            menu.add_command(label="Start Pomodoro", command=lambda: self.__set_state("sad_pomodoro"))
+            print("sad_idle")
+        else: # idle
+            menu.add_command(label="Start Pomodoro", command=lambda: self.__set_state("pomodoro"))
+            print("idle")
+        menu.add_separator()
+        menu.add_command(label="Close", command=pet_window.quit)
+        menu.post(event.x_root, event.y_root)
+
+
 pet_window = tk.Tk()
-state = "sad_pomodoro"
-gif = None
-
-# call buddy's action gif
-raccoon_knitting = [tk.PhotoImage(file=impath + 'raccoon_knitting.gif', format='gif -index %i' % (i)) for i in range(2)]
-raccoon_idle = [tk.PhotoImage(file=impath + 'raccoon_idle.gif', format='gif -index %i' % (i)) for i in range(2)]
-raccoon_sad_idle = [tk.PhotoImage(file=impath + 'raccoon_sad.gif', format='gif -index %i' % (i)) for i in range (3)]
-raccoon_sad_knitting = [tk.PhotoImage(file=impath + 'raccoon_sad_knitting.gif', format='gif -index %i' % (i)) for i in range (2)]
-
-# making gif loop through frames
-frame = 0
-def play_gif(frames):
-    global frame
-    pet_label.configure(image=frames[frame])
-    frame = (frame + 1) % len(frames)
-    delay = 1000
-    if state == "sad_pomodoro":
-        delay = 1800
-    elif state == "sad_idle":
-        delay = 500
-    pet_window.after(delay, play_gif, frames)
-
-def on_right_click(event):
-    print("RIGHT CLICK detected")  # test line
-
-    # Create popup menu
-    menu = tk.Menu(pet_window, tearoff=0)
-    # menu.add_command(label="Sleep", command=lambda: change_state(1, 5))
-    if state == "pomodoro" or state == "sad_pomodoro":
-        menu.add_command(label="Pause Pomodoro", command=pet_window.quit)
-        menu.add_command(label="End Pomodoro", command=pet_window.quit)
-    else: #for idle and sad idle
-        menu.add_command(label="Start Pomodoro", command=pet_window.quit)
-    menu.add_separator()
-    menu.add_command(label="Close", command=pet_window.quit)
-    menu.post(event.x_root, event.y_root)
-
-#window and label configuration and binding
-pet_window.geometry(str(gif_dimension)+ 'x' +str(gif_dimension) + '+' + str(x) + '+' + str(y))  # dimensions and positioning of pet
-pet_window.overrideredirect(True)
-pet_window.wm_attributes('-transparentcolor', 'red')
-pet_window.wm_attributes('-topmost', True)  # Make it top of everything
-
-pet_label = tk.Label(pet_window, bd=0, bg='black')
-pet_label.bind('<Button-3>', on_right_click)  # Right click
-pet_label.pack()
-
-# play gifs
-if state == "pomodoro":
-    gif = raccoon_knitting
-elif state == "idle":
-    gif = raccoon_idle
-elif state == "sad_idle":
-    gif = raccoon_sad_idle
-elif state == "sad_pomodoro":
-    gif = raccoon_sad_knitting
-play_gif(gif)
+raccoon = RaccoonWindow(pet_window)
 pet_window.mainloop()
 
